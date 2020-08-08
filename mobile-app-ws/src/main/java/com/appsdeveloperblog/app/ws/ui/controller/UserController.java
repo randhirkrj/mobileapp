c00647ec2a78b400e34983cd7d68a9c4f06d46a7
randhir <randhir_kr_jha@rediffmail.com>
2020-08-08 18:04:01 +0530

    third commit Mobile app

---
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController 
+{
+	@GetMapping
+	public String getUser() 
+	{
+		return "Get User was called";
+	} 
+	
+	@PostMapping 
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) 
+	{
+		return null;
+	}
+    
+	@PutMapping 
+	public String updateUser() 
+	{
+		return " Update User was called";
+	}
+	@DeleteMapping 
+	public String deleteUser() 
+	{
+		return " DeleteUser was called";
+	}
+	
+}
